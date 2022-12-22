cb77eba392108e7b40cead31105fbbea7cd90d01
joaombpereira-eng <joaopereira.eng.tec@gmail.com>
2022-12-22 10:18:07 +0000

    Editing Place Items and their List

---
+++ components/Places/PlaceItem.js
@@ -0,0 +1,17 @@
+import {Image, Text, View, StyleSheet, Pressable} from 'react-native';
+
+export default function PlaceItem({place, onSelect}) {
+  return (
+    <Pressable onPress={onSelect}>
+      <Image source={{uri: place.imageUri}} />
+      <View>
+        <Text>{place.title}</Text>
+        <Text>{place.address}</Text>
+      </View>
+    </Pressable>
+  );
+}
+
+const styles = StyleSheet.create({
+	
+});
