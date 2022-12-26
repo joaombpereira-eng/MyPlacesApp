f05131f74df91e334cdfe5cbead95cad0097614a
joaombpereira-eng <joaopereira.eng.tec@gmail.com>
2022-12-26 10:28:55 +0000

    Entered Data Passed to AllPlacesScreen

--- components/Places/PlaceForm.js
+++ components/Places/PlaceForm.js
@@ -4,8 +4,9 @@ import {Colors} from '../../constants/colors';
 import Button from '../ui/Button';
 import ImagePicker from './ImagePicker';
 import LocationPicker from './LocationPicker';
+import {Place} from '../../models/place';
 
-export default function PlaceForm() {
+export default function PlaceForm({onCreatePlace}) {
   const [enteredTitle, setEnteredTitle] = useState('');
   const [selectedImage, setSelectedImage] = useState();
   const [pickedLocation, setPickedLocation] = useState();
@@ -23,10 +24,8 @@ export default function PlaceForm() {
   }, []);
 
   function savePlaceHandler() {
-    console.log('Title');
-    console.log(enteredTitle);
-    console.log(selectedImage);
-    console.log(pickedLocation);
+    const placeData = new Place(enteredTitle, selectedImage, pickedLocation);
+    onCreatePlace(placeData);
   }
 
   return (
